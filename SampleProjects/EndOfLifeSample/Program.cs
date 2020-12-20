a4a9f57b19eec332af7cba1f0c12143a7c7b80bb
Doug Murphy <doug.murphy82@gmail.com>
2020-12-20 17:11:27 -0500

    Add new sample project for EOL warning.

---
+++ SampleProjects/EndOfLifeSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EndOfLifeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
