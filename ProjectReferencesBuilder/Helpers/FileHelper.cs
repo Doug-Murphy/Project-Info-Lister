1967c37ab7c005fd3f80fd992977e15ab370c772
Doug Murphy <doug.murphy82@gmail.com>
2020-12-23 15:42:44 -0500

    Cache file contents to reduce disk IO.

--- ProjectReferencesBuilder/Helpers/FileHelper.cs
+++ ProjectReferencesBuilder/Helpers/FileHelper.cs
@@ -6,13 +6,22 @@ namespace ProjectReferencesBuilder.Helpers
 {
     public static class FileHelper
     {
+        private static readonly Dictionary<string, IEnumerable<string>> _cachedFileContents = new Dictionary<string, IEnumerable<string>>();
+
         public static IEnumerable<string> GetFileContents(string filePath)
         {
+            if (_cachedFileContents.ContainsKey(filePath))
+            {
+                return _cachedFileContents[filePath];
+            }
+
             var fileContents = File.ReadAllLines(filePath);
             if (fileContents == null || fileContents.Length == 0)
             {
                 throw new InvalidOperationException("The project file path is an empty file.");
             }
+
+            _cachedFileContents.Add(filePath, fileContents);
 
             return fileContents;
         }
