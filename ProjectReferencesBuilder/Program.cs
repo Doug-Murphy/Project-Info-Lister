09cf1c337779010267180bcf04d645f8bedb7d75
Doug Murphy <doug.murphy82@gmail.com>
2021-01-03 10:39:18 -0500

    Change how DI is done.
    * Make it more akin to a web app.
    * Uses some pre-configured defaults so that we get some more inherent out of the box functionality automatically added in.
    * Able to now use constructor for Program.cs

--- ProjectReferencesBuilder/Program.cs
+++ ProjectReferencesBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ProjectReferencesBuilder.Entities.Models;
 using ProjectReferencesBuilder.Helpers;
 using ProjectReferencesBuilder.Helpers.Interface.WarningHelpers;
@@ -12,35 +13,40 @@ using System.Text.RegularExpressions;
 
 namespace ProjectReferencesBuilder
 {
-    class Program
+    public class Program
     {
-        private static void WriteResultsToConsole(ResultsOutput results)
+        private readonly IProjectInfoService _projectInfoService;
+        private readonly IWarningsService _warningsService;
+
+        public Program(IProjectInfoService projectInfoService,
+                       IWarningsService warningsService)
         {
-            Console.WriteLine(JsonSerializer.Serialize(results, new JsonSerializerOptions { WriteIndented = true }));
+            _projectInfoService = projectInfoService;
+            _warningsService = warningsService;
         }
 
-        private static void WriteResultsToFile(string solutionFilePath, ResultsOutput finalOutput)
+        private static IHostBuilder CreateHostBuilder(string[] args)
         {
-            var solutionName = FileHelper.GetFileName(solutionFilePath);
-            var fileName = $"{solutionName}_{DateTime.Now}";
-            var invalidChars = new string(Path.GetInvalidFileNameChars());
-            var regExReplacer = new Regex($"[{Regex.Escape(invalidChars)}]");
-            fileName = regExReplacer.Replace(fileName, "_") + ".json";
-            File.WriteAllText(fileName, JsonSerializer.Serialize(finalOutput, new JsonSerializerOptions { WriteIndented = true }));
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<Program>();
+                    services.AddSingleton<IEndOfLifeWarningHelper, EndOfLifeWarningHelper>();
+                    services.AddSingleton<IProjectStyleWarningHelper, ProjectStyleWarningHelper>();
+                    services.AddSingleton<IProjectInfoService, ProjectInfoService>();
+                    services.AddSingleton<IWarningsService, WarningsService>();
+                });
         }
  
         static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection()
-                .AddSingleton<IEndOfLifeWarningHelper, EndOfLifeWarningHelper>()
-                .AddSingleton<IProjectStyleWarningHelper, ProjectStyleWarningHelper>()
-                .AddSingleton<IProjectInfoService, ProjectInfoService>()
-                .AddSingleton<IWarningsService, WarningsService>()
-                .BuildServiceProvider();
+            var host = CreateHostBuilder(args).Build();
 
-            var projectInfoService = serviceProvider.GetService<IProjectInfoService>();
-            var warningsService = serviceProvider.GetService<IWarningsService>();
+            host.Services.GetRequiredService<Program>().Run(args);
+        }
 
+        public void Run(string[] args)
+        {
             string solutionFilePath = "";
             if (args.Length == 1)
             {
@@ -55,8 +61,8 @@ namespace ProjectReferencesBuilder
 
             solutionFilePath = solutionFilePath.Trim('"');
 
-            var projectsWithInfo = projectInfoService.WithName().WithReferences().WithTfm().GetInfo(solutionFilePath);
-            var warningsForProjects = warningsService.GetWarnings(projectsWithInfo);
+            var projectsWithInfo = _projectInfoService.WithName().WithReferences().WithTfm().GetInfo(solutionFilePath);
+            var warningsForProjects = _warningsService.GetWarnings(projectsWithInfo);
             var finalOutput = new ResultsOutput
             {
                 ProjectsWithInfo = projectsWithInfo,
@@ -65,6 +71,21 @@ namespace ProjectReferencesBuilder
 
             WriteResultsToConsole(finalOutput);
             //WriteResultsToFile(solutionFilePath, finalOutput);
+        }
+
+        private static void WriteResultsToConsole(ResultsOutput results)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(results, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        private static void WriteResultsToFile(string solutionFilePath, ResultsOutput finalOutput)
+        {
+            var solutionName = FileHelper.GetFileName(solutionFilePath);
+            var fileName = $"{solutionName}_{DateTime.Now}";
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            var regExReplacer = new Regex($"[{Regex.Escape(invalidChars)}]");
+            fileName = regExReplacer.Replace(fileName, "_") + ".json";
+            File.WriteAllText(fileName, JsonSerializer.Serialize(finalOutput, new JsonSerializerOptions { WriteIndented = true }));
         }
     }
 }
