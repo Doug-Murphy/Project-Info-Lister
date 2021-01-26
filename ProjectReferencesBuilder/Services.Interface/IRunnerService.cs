adb102f29278ebaf3889ce38c2037d85cb969415
Doug Murphy <doug.murphy82@gmail.com>
2021-01-26 13:09:04 -0500

    Refactor Program.cs
    * Extract all possible logic into a new RunnerService.cs for unit testing.

---
+++ ProjectReferencesBuilder/Services.Interface/IRunnerService.cs
@@ -0,0 +1,13 @@
+﻿using ProjectReferencesBuilder.Entities.Models;
+
+namespace ProjectReferencesBuilder.Services.Interface
+{
+    public interface IRunnerService
+    {
+        ResultsOutput GetProjectDetails(string solutionFilePath);
+
+        void WriteResultsToConsole(ResultsOutput results);
+
+        void WriteResultsToFile(string solutionFilePath, ResultsOutput finalOutput);
+    }
+}
