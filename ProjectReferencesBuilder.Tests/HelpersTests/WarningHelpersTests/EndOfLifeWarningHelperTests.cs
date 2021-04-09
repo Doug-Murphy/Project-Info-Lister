07ed2bca8169c8e4a1039635b975e0931f4fbbe6
Doug Murphy <doug.murphy82@gmail.com>
2021-04-08 20:53:09 -0400

    Convert warning helper tests to XUnit.

--- ProjectReferencesBuilder.Tests/HelpersTests/WarningHelpersTests/EndOfLifeWarningHelperTests.cs
+++ ProjectReferencesBuilder.Tests/HelpersTests/WarningHelpersTests/EndOfLifeWarningHelperTests.cs
@@ -1,31 +1,29 @@
-﻿using NUnit.Framework;
-using ProjectReferencesBuilder.Entities.Models;
+﻿using ProjectReferencesBuilder.Entities.Models;
 using ProjectReferencesBuilder.Helpers.WarningHelpers;
 using System.Collections.Generic;
+using Xunit;
 
 namespace ProjectReferencesBuilder.Tests.HelpersTests.WarningHelpersTests
 {
-    [Parallelizable(ParallelScope.All)]
     public class EndOfLifeWarningHelperTests
     {
         private const string _mockedAbsolutePath = @"dummy/absolute/path.csproj";
-        private static IEnumerable<TestCaseData> TestIsProjectTfmEndOfLife_TestCases
+        public static IEnumerable<object[]> TestIsProjectTfmEndOfLife_TestCases()
         {
-            get
-            {
-                yield return new TestCaseData(new ProjectInfo(_mockedAbsolutePath) { TFM = "netcoreapp2.2" }).Returns(true);
-                yield return new TestCaseData(new ProjectInfo(_mockedAbsolutePath) { TFM = "netcoreapp2.2;netcoreapp3.1" }).Returns(true);
-                yield return new TestCaseData(new ProjectInfo(_mockedAbsolutePath) { TFM = "net5.0" }).Returns(false);
-                yield return new TestCaseData(new ProjectInfo(_mockedAbsolutePath) { TFM = "unsupported_TFM" }).Returns(false);
-            }
+            yield return new object[] { true, new ProjectInfo(_mockedAbsolutePath) { TFM = "netcoreapp2.2" } };
+            yield return new object[] { true, new ProjectInfo(_mockedAbsolutePath) { TFM = "netcoreapp2.2;netcoreapp3.1" } };
+            yield return new object[] { false, new ProjectInfo(_mockedAbsolutePath) { TFM = "net5.0" } };
+            yield return new object[] { false, new ProjectInfo(_mockedAbsolutePath) { TFM = "unsupported_TFM" } };
         }
-        [TestCaseSource(nameof(TestIsProjectTfmEndOfLife_TestCases))]
-        public bool TestIsProjectTfmEndOfLife(ProjectInfo project)
+
+        [Theory]
+        [MemberData(nameof(TestIsProjectTfmEndOfLife_TestCases))]
+        public void TestIsProjectTfmEndOfLife(bool expectedResult, ProjectInfo project)
         {
             var mockEndOfLifeWarningHelper = new EndOfLifeWarningHelper();
             var isEol = mockEndOfLifeWarningHelper.IsProjectTfmEndOfLife(project, out string _);
 
-            return isEol;
+            Assert.Equal(expectedResult, isEol);
         }
     }
 }
