f81d66ce89cb3e5937f263dc73054a8324c04ab7
Doug Murphy <doug.murphy82@gmail.com>
2022-02-24 21:41:07 -0500

    Use top-level statement.

--- ProjectReferencesBuilder/Program.cs
+++ ProjectReferencesBuilder/Program.cs
@@ -6,44 +6,36 @@ using ProjectReferencesBuilder.Helpers.WarningHelpers;
 using ProjectReferencesBuilder.Services;
 using ProjectReferencesBuilder.Services.Interface;
 
-namespace ProjectReferencesBuilder;
-
-public class Program
+static IHostBuilder CreateHostBuilder(string[] args)
 {
-    private static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureServices(services =>
-            {
-                services.AddSingleton<IEndOfLifeWarningHelper, EndOfLifeWarningHelper>();
-                services.AddSingleton<IProjectInfoService, ProjectInfoService>();
-                services.AddSingleton<IProjectStyleWarningHelper, ProjectStyleWarningHelper>();
-                services.AddSingleton<IRunnerService, RunnerService>();
-                services.AddSingleton<IWarningsService, WarningsService>();
-            });
-    }
- 
-    static void Main(string[] args)
-    {
-        var host = CreateHostBuilder(args).Build();
-        var runnerService = host.Services.GetRequiredService<IRunnerService>();
-
-        string solutionFilePath = null;
-        if (args.Length == 1)
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
         {
-            solutionFilePath = args[0];
-        }
-
-        while (FileHelper.GetFileExtension(solutionFilePath) != ".sln")
-        {
-            Console.Write("Enter the full path to the solution (.sln) file: ");
-            solutionFilePath = Console.ReadLine();
-        }
-
-        solutionFilePath = solutionFilePath.Trim('"');
-
-        var results = runnerService.GetProjectDetails(solutionFilePath);
-
-        runnerService.WriteResultsToConsole(results);
-    }
+            services.AddSingleton<IEndOfLifeWarningHelper, EndOfLifeWarningHelper>();
+            services.AddSingleton<IProjectInfoService, ProjectInfoService>();
+            services.AddSingleton<IProjectStyleWarningHelper, ProjectStyleWarningHelper>();
+            services.AddSingleton<IRunnerService, RunnerService>();
+            services.AddSingleton<IWarningsService, WarningsService>();
+        });
 }
+
+var host = CreateHostBuilder(args).Build();
+var runnerService = host.Services.GetRequiredService<IRunnerService>();
+
+string solutionFilePath = null;
+if (args.Length == 1)
+{
+    solutionFilePath = args[0];
+}
+
+while (FileHelper.GetFileExtension(solutionFilePath) != ".sln")
+{
+    Console.Write("Enter the full path to the solution (.sln) file: ");
+    solutionFilePath = Console.ReadLine();
+}
+
+solutionFilePath = solutionFilePath.Trim('"');
+
+var results = runnerService.GetProjectDetails(solutionFilePath);
+
+runnerService.WriteResultsToConsole(results);
